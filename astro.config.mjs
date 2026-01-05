e2dcb47ad359f2034f45095570e34b121bfe498b
ondrui59 <ondrui59@gmail.com>
2026-01-05 12:15:57 +0000

    added dark theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://9000-firebase-my-app-1767346414653.cluster-jgdkb37mtnfb4urxtja5guzqog.cloudworkstations.dev",
+  integrations: [preact()],
 });
